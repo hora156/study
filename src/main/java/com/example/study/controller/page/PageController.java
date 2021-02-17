b19e037d37167b70cc19b831b7952779a28b7c86
name <god@god.com>
2021-02-17 09:44:07 +0900

    02-17 master update(start front-end)

--- src/main/java/com/example/study/controller/page/PageController.java
+++ src/main/java/com/example/study/controller/page/PageController.java
@@ -1,11 +1,13 @@
 package com.example.study.controller.page;
 
 import com.example.study.service.AdminMenuService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 @RequestMapping("/pages")
 public class PageController {
@@ -13,8 +15,9 @@ public class PageController {
     @Autowired
     private AdminMenuService adminMenuService;
 
-    @RequestMapping(path = {""})
+    @RequestMapping(path = {"", "/main"})
     public ModelAndView index() {
+      log.info("{}", adminMenuService.getAdminMenu());
         return new ModelAndView("/pages/main")
                 .addObject("menuList", adminMenuService.getAdminMenu())
                 .addObject("code", "main")
@@ -23,6 +26,7 @@ public class PageController {
 
     @RequestMapping("/user")
     public ModelAndView user() {
+        log.info("{}", adminMenuService.getAdminMenu());
         return new ModelAndView("/pages/user")
                 .addObject("menuList", adminMenuService.getAdminMenu())
                 .addObject("code", "user")
